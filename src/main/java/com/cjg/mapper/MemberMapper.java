92dd7816fee46710e3a63b80aa8c8773d7262a77
coolcjg <coolcjg@naver.com>
2019-05-13 18:55:18 +0900

    2019-05-13-chap33-end

---
+++ src/main/java/com/cjg/mapper/MemberMapper.java
@@ -0,0 +1,8 @@
+package com.cjg.mapper;
+
+import com.cjg.domain.MemberVO;
+
+public interface MemberMapper {
+	public MemberVO read(String userid);
+
+}
---
+++ src/main/java/com/cjg/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.cjg.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.cjg.domain.MemberVO;
+import com.cjg.mapper.MemberMapper;
+import com.cjg.security.domain.CustomUser;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Setter(onMethod_= {@Autowired})
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+		log.warn("Load User By UserName : " + userName);
+		
+		MemberVO vo = memberMapper.read(userName);
+		
+		log.warn("queried by member mapper: " + vo);
+		
+		return vo == null ? null : new CustomUser(vo);
+	}
+
+}
